4fbe2f022970716d47854c8e539fb912ea6096e9
Adam Gray <adamgray@Adams-MacBook-Pro.local>
2023-09-19 13:29:53 -0500

    fix image bug

--- components/ProductListing.js
+++ components/ProductListing.js
@@ -1,12 +1,19 @@
 import Link from 'next/link';
 
 export default function ProductListing({ product }) {
-  let image = product.images.edges[0].node;
+  let image = null;
+
+  if(product.images.edges[0]) {
+    image = product.images.edges[0].node;
+  }
+
   return (
     <li className="product-card">
-      <div className="product-card-frame">
-        <img className="prodimg" src={image.src} alt={image.altText} />
-      </div>
+      {image && (
+        <div className="product-card-frame">
+          <img className="prodimg" src={image.src} alt={image.altText} />
+        </div>
+      )}
       <div className="product-card-text">
         <h3 className="product-card-title">{product.title}</h3>
         <p className="product-card-description">
